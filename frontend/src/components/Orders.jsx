9489fb75827aeae97985a27172d7c6d84cdaf264
Aspectzxcc <josh10nathan@gmail.com>
2025-04-01 17:12:25 +0800

    formatted order statuses in customer orders page

--- frontend/src/components/Orders.jsx
+++ frontend/src/components/Orders.jsx
@@ -87,7 +87,12 @@ function Orders() {
                                                                 'text.primary'
                                                     }}
                                                 >
-                                                    {order.status}
+                                                    {order.status
+                                                        .replace(/_/g, ' ')
+                                                        .split(' ')
+                                                        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                                                        .join(' ')
+                                                    }
                                                 </Typography>
                                             </Grid>
                                         </Grid>
